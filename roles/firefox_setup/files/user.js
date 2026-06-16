74a40f11565d144218ac419ec5d97f8a9e89b193
Lucas Furtado Carregã <lfcarrega@gmail.com>
2026-06-16 14:09:03 +0000

    add firefox_setup role

---
+++ roles/firefox_setup/files/user.js
@@ -0,0 +1,4 @@
+user_pref("browser.ml.enable", false);
+user_pref("browser.ml.chat.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("toolkit.telemetry.enabled", false);
